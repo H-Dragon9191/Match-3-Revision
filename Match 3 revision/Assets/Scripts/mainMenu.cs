e89e722dcce6b552e18c1e53b5ac084ade8bc89f
Your Name <you@example.com>
2025-08-29 18:23:01 +0300

    uploading everything

--- Match 3 revision/Assets/Scripts/mainMenu.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class mainMenu : MonoBehaviour
-{
-    public void Play(int level)
-    {
-        SceneManager.LoadScene("Level_" + level);
-    }
-}
